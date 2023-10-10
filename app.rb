45b3feb12761a7276c215f138f6453fc86c3e9d1
Michael Sjöberg <mail@michaelsjoeberg.com>
2023-10-10 09:11:50 +0200

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,5 +1,10 @@
 require "sinatra"
+# auto-reload
+require "sinatra/reloader"
+
+set :views, File.join(settings.root, "views")
 
 get "/" do
-	"Hello Sinatra"
+	@greeting = "hello sinatra"
+	erb :index
 end
