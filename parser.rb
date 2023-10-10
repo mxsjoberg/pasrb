242ec7d3715ab968dffa4c446e7fe2052edcd7a0
Michael Sjöberg <mail@michaelsjoeberg.com>
2023-10-10 10:32:26 +0200

    Update parser.rb

--- parser.rb
+++ parser.rb
@@ -1,6 +1,28 @@
 # parser.rb
 
 def parse(text)
-	# TODO: parse text input and return AST
-	text
+    # TODO: parse text input and return AST
+    text_length = text.length
+    ch_index = 0
+    ch_current = ""
+
+    tokens = Array.new
+
+    while ch_index < text_length
+        ch_current = text[ch_index]
+
+        case ch_current
+        when /\d/
+            number = ch_current
+            tokens << { number: number, col: ch_index }
+        end
+
+        # push to tokens
+        # tokens << ch_current
+        ch_index += 1
+    end
+
+    puts tokens
+
+    tokens
 end
