6541006f3cb139288d7049bf92213ab84c0b159a
Michael Sjöberg <mail@michaelsjoeberg.com>
2023-10-10 09:34:46 +0200

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require "sinatra"
+require "json"
 # auto-reload
 require "sinatra/reloader"
 
@@ -7,4 +8,14 @@ set :views, File.join(settings.root, "views")
 get "/" do
     @greeting = "hello sinatra"
     erb :index
+end
+
+post "/update" do
+    content_type :json
+    # parse
+    request_body = JSON.parse(request.body.read)
+    # process
+    data = request_body
+    # return
+    { message: "ok", data: data["input"] }.to_json
 end
