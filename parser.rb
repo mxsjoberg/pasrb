0262c4ccca42ae010259a409846ff0234ef80df1
Michael Sjöberg <mail@michaelsjoeberg.com>
2023-10-10 12:59:41 +0200

    Update parser.rb

--- parser.rb
+++ parser.rb
@@ -1,28 +1,101 @@
 # parser.rb
 
+require 'pp'
+
+# expr      ::= term ((+ | -) term)*
+# term      ::= factor ((* | /) factor)*
+# factor    ::= number
+
+def parse_expr(tokens, tk_index)
+    expr = Array.new
+
+    # term
+    term, tk_index = parse_term(tokens, tk_index)
+    expr = term
+
+    # (+ | -)
+    while tk_index < tokens.length && (tokens[tk_index]["value".to_sym] == "+" || tokens[tk_index]["value".to_sym] == "-")
+        tk_current = tokens[tk_index]
+        tk_index += 1
+
+        # term
+        begin
+            term, tk_index = parse_term(tokens, tk_index)
+            expr = [tk_current["value".to_sym], [expr, term]]
+        rescue
+        end
+    end
+
+    return expr, tk_index
+end
+
+def parse_term(tokens, tk_index)
+    term = Array.new
+
+    # factor
+    factor, tk_index = parse_factor(tokens, tk_index)
+    term = factor
+
+    # (* | /)
+    while tk_index < tokens.length && (tokens[tk_index]["value".to_sym] == "*" || tokens[tk_index]["value".to_sym] == "/")
+        tk_current = tokens[tk_index]
+        tk_index += 1
+        
+        # factor
+        begin
+            factor, tk_index = parse_factor(tokens, tk_index)
+            term = [tk_current["value".to_sym], [term, factor]]
+        rescue
+        end
+    end
+
+    return term, tk_index
+end
+
+def parse_factor(tokens, tk_index)
+    factor = Array.new
+
+    tk_current = tokens[tk_index]
+    tk_index += 1
+
+    # number
+    case tk_current["type".to_sym]
+    when "number"
+        factor = tk_current["value".to_sym]
+    end
+
+    return factor, tk_index
+end
+
 def parse(text)
-    # TODO: parse text input and return AST
-    text_length = text.length
-    ch_index = 0
-    ch_current = ""
-
+    # tokens
     tokens = Array.new
+    ch_index = 0
 
-    while ch_index < text_length
+    while ch_index < text.length
         ch_current = text[ch_index]
 
         case ch_current
         when /\d/
             number = ch_current
-            tokens << { number: number, col: ch_index }
+            tokens << { type: "number", value: number, col: ch_index }
+        when /[\+\-\*\/]/
+            operator = ch_current
+            tokens << { type: "operator", value: operator, col: ch_index }
         end
 
-        # push to tokens
-        # tokens << ch_current
         ch_index += 1
     end
 
-    puts tokens
+    # ast
+    ast = Array.new
+    tk_index = 0
 
-    tokens
+    while tk_index < tokens.length
+        expr, tk_index = parse_expr(tokens, tk_index)
+        ast = expr
+    end
+
+    # pp ast
+    ast
 end
