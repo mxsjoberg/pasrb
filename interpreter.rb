df3e6dab7437904d79cc37e7df928ac3e9bf9775
Michael Sjöberg <mail@michaelsjoeberg.com>
2023-10-13 09:44:22 +0200

    Update interpreter.rb

--- interpreter.rb
+++ interpreter.rb
@@ -3,7 +3,7 @@
 # TODO: add rest of statements
 
 def interpret(ast)
-    result = nil
+    # result = nil
     node = ast
 
     if node.kind_of?(Array)
@@ -16,19 +16,24 @@ def interpret(ast)
 
     case left
     when /\+/
-        result = interpret(right[0]).to_i + interpret(right[1]).to_i
+        return interpret(right[0]).to_i + interpret(right[1]).to_i
     when /\-/
-        result = interpret(right[0]).to_i - interpret(right[1]).to_i
+        return interpret(right[0]).to_i - interpret(right[1]).to_i
     when /\*/
-        result = interpret(right[0]).to_i * interpret(right[1]).to_i
+        return interpret(right[0]).to_i * interpret(right[1]).to_i
     when /\//
-        result = interpret(right[0]).to_i / interpret(right[1]).to_i
+        return interpret(right[0]).to_i / interpret(right[1]).to_i
     when /\d/
         return left
     when "assignment"
-
+        $symbols[right[0].to_sym] = interpret(right[1])
+        return
+    when "output"
+        $output << interpret(right)
+        return
+    when /[a-zA-Z]/
+        return $symbols[left.to_sym]
     end
 
     # result ? result : "nil"
-    result
 end
